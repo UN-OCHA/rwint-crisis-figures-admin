e0baf88ba8208bc87d254a3359ad6695a260b93a
Bushra Addlan <bushra.addlan@gmail.com>
2020-02-17 21:20:14 +0300

    refactor(entities): create `country` entity

---
+++ src/app/core/api/entities/country.ts
@@ -0,0 +1,28 @@
+import { JsonObject, JsonProperty } from 'json2typescript';
+import { Entity } from '@core/api/entities/entity';
+
+/**
+ * Country entity.
+ */
+@JsonObject('Country')
+export class Country extends Entity {
+  @JsonProperty('id') // Type can be ignored or set to `Any` for optional properties.
+  id: number = undefined;
+
+  @JsonProperty('code', String)
+  code: string = undefined;
+
+  @JsonProperty('name', String)
+  name: string = undefined;
+
+  /** @override */
+  getIdentifierProperty(): string {
+    return 'code';
+  }
+
+  /** @override */
+  toString(): string {
+    return this.name;
+  }
+}
+
