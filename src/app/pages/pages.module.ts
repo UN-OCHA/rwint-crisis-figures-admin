aa941fa94c5dfa5f563680f3d422f42dd3ce1e8e
Bushra Addlan <bushra.addlan@gmail.com>
2020-02-21 00:02:44 +0300

    refactor(pages): import `miscellaneous` module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,12 +4,14 @@ import { NbMenuModule } from '@nebular/theme';
 import { ThemeModule } from '../theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 
 @NgModule({
   imports: [
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
+    MiscellaneousModule,
   ],
   declarations: [
     PagesComponent,
