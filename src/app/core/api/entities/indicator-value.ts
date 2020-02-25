b05ff17f2252722f7873fe2dc7eef899408dcb65
Bushra Addlan <bushra.addlan@gmail.com>
2020-02-25 23:47:08 +0300

    refactor(indicator-value): add `IndicatorValue` entity

---
+++ src/app/core/api/entities/indicator-value.ts
@@ -0,0 +1,31 @@
+import { Any, JsonObject, JsonProperty } from 'json2typescript';
+import { Entity } from '@core/api/entities/entity';
+import { Indicator } from '@core/api/entities/indicator';
+
+/**
+ * IndicatorValue entity.
+ */
+@JsonObject('IndicatorValue')
+export class IndicatorValue extends Entity {
+  static PLURAL_NAME = 'values';
+
+  @JsonProperty('id') // Type can be ignored or set to `Any` for optional properties.
+  id: number = undefined;
+
+  @JsonProperty('value', Number)
+  value: number = undefined;
+
+  @JsonProperty('date', String)
+  date: string = undefined;
+
+  @JsonProperty('sourceUrl', String)
+  sourceUrl: string = undefined;
+
+  @JsonProperty('indicator', Indicator, true) // Optional property
+  indicator: Indicator = undefined;
+
+  /** @override */
+  toString(): string {
+    return `Value: ${this.value}`;
+  }
+}
