a7d94013cad1e1074adcc1c2430c3ccb7a60b10d
Bushra Addlan <bushra.addlan@gmail.com>
2020-02-29 16:41:43 +0300

    refactor(indicator-values-list): load local Indicator instance using route parameter

--- src/app/pages/entities/indicator-values-list/indicator-values-list.component.ts
+++ src/app/pages/entities/indicator-values-list/indicator-values-list.component.ts
@@ -1,10 +1,14 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, Injector, OnDestroy, OnInit } from '@angular/core';
 import moment from 'moment';
-import { IndicatorValueService } from '@core/api';
+import isNumber from 'lodash/isNumber';
+import { IndicatorValueService, IndicatorService } from '@core/api';
 import { EntityConstructor } from '@core/api/types';
 import { IndicatorValue } from '@core/api/entities/indicator-value';
 import { EntitiesListComponent } from '@pages/entities/entities-list.component';
 import { IndicatorValuesFormComponent } from '@pages/entities/indicator-values-form/indicator-values-form.component';
+import { idFromIri, isIri } from '@core/utils/entity.util';
+import { HttpParams } from '@angular/common/http';
+import { Indicator } from '@core/api/entities/indicator';
 
 @Component({
   selector: 'ngx-indicator-values-list',
@@ -13,16 +17,41 @@ import { IndicatorValuesFormComponent } from '@pages/entities/indicator-values-f
 })
 export class IndicatorValuesListComponent extends EntitiesListComponent<IndicatorValue> implements OnInit {
 
+  /** Related `Indicator` instance */
+  indicator?: Indicator;
+
   /** Constructor */
   constructor(protected injector: Injector,
-              protected entityService: IndicatorValueService) {
+              protected entityService: IndicatorValueService,
+              protected indicatorService: IndicatorService) {
     super(injector);
     this.entityFormComponent = IndicatorValuesFormComponent;
   }
 
   /** @override */
   ngOnInit(): void {
+    // Validate route parameter representing Indicator ID and load it
+    if (this.route.snapshot.params.id) {
+      // Load `Indicator` instance using `id` param
+      this.loadIndicator(Number(this.route.snapshot.params.id));
+      // Filter list by aforementioned `Indicator` ID
+      this.listRequestParams = this.listRequestParams.set('indicator.id', this.route.snapshot.params.id)
+    }
+
     super.ngOnInit();
+  }
+
+  /**
+   * Load related `Indicator` instance.
+   *
+   * @param indicatorId
+   */
+  protected loadIndicator(indicatorId: number) {
+    const params = new HttpParams().set('with[]', 'country');
+
+    this.indicatorService.one(indicatorId, params).subscribe(responseCtx => {
+      this.indicator = responseCtx.body;
+    });
   }
 
   /** @override */
