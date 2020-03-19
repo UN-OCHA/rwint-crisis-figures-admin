6f6525c9939f3f46a66bd9323ac30b603736fb4c
Bushra Addlan <bushra.addlan@gmail.com>
2020-03-19 03:15:50 +0300

    refactor(vocabulary-form): allow automatic/manual editing of `name` field

--- src/app/pages/entities/vocabularies-form/vocabularies-form.component.ts
+++ src/app/pages/entities/vocabularies-form/vocabularies-form.component.ts
@@ -1,11 +1,14 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { NbDialogRef } from '@nebular/theme';
-import { EntitiesFormComponent } from '@pages/entities/entities-form.component';
+import { EntitiesFormComponent, FormMode } from '@pages/entities/entities-form.component';
 import { Country } from '@core/api/entities/country';
 import { Vocabulary } from '@core/api/entities/vocabulary';
 import { VocabularyService } from '@core/api';
 import { AutocompleteSearchDelegate } from '@core/api/types';
+import { generateIri, isIri, slugify } from '@core/utils/entity.util';
+import isObject from 'lodash/isObject';
+import { Term } from '@core/api/entities/term';
 
 @Component({
   selector: 'ngx-vocabularies-form',
@@ -14,10 +17,8 @@ import { AutocompleteSearchDelegate } from '@core/api/types';
 })
 export class VocabulariesFormComponent extends EntitiesFormComponent<Vocabulary> implements OnInit {
 
-  entityConstructor = Vocabulary;
-
-  /** */
-  countryAcSearchDelegate: AutocompleteSearchDelegate<Country>;
+  /** The state of `name` field editablility */
+  enableManualNameEntry: boolean = false;
 
   /** Constructor */
   constructor(protected injector: Injector,
@@ -37,11 +38,24 @@ export class VocabulariesFormComponent extends EntitiesFormComponent<Vocabulary>
   /** @override */
   ngOnInit(): void {
     super.ngOnInit();
+
+    // Enable manual name entry by default in `Update` mode
+    this.enableManualNameEntry = this.formMode === FormMode.UPDATE;
+
+    // Track changes to `label` field to slugfiy and update `name` field accordingly
+    this.observe(this.label.valueChanges).subscribe({
+      next: labelValue => {
+        if (!this.enableManualNameEntry && this.label.dirty) {
+          this.updateNameFromLabel();
+        }
+      },
+    });
   }
 
   getEntityConstructor() {
     return Vocabulary;
   }
+
   // // //  Accessors
 
   get name() {
@@ -49,6 +63,58 @@ export class VocabulariesFormComponent extends EntitiesFormComponent<Vocabulary>
   }
   get label() {
     return this.entityForm.get('label');
+  }
+
+  // // //
+
+  /**
+   * Set the `name` field value from the `label` value after slugifying it.
+   */
+  protected updateNameFromLabel() {
+    if (this.label.value) {
+      this.name.setValue(slugify(this.label.value));
+
+      if (this.name.pristine) {
+        this.name.markAsDirty();
+      }
+    }
+  }
+
+  /** @override */
+  protected normalizeFormValues(): any {
+    const values = super.normalizeFormValues();
+
+    // Set the `name` property in the normalized data if it is not already
+    // present in form values
+    if (!values.name) {
+      values.name = this.name.value;
+    }
+
+    // Attempt to format related `vocabulary` instance as IRI string;
+    // otherwise, remove `vocabulary` property as FormMode.UPDATE is implied.
+    if (isObject(values.vocabulary) && values.vocabulary.id) {
+      if (values.vocabulary instanceof Vocabulary) {
+        values.vocabulary = generateIri(values.vocabulary);
+      } else if (isIri(values.vocabulary['@id'])) {
+        values.vocabulary = values.vocabulary['@id'];
+      } else {
+        delete values.vocabulary;
+      }
+    }
+
+    return values;
+  }
+
+  /** @override */
+  protected populateForm(entity: Vocabulary) {
+    super.populateForm(entity);
+
+    // Ensure the `name` field is initially disabled
+    setTimeout(() => {
+      if (!this.enableManualNameEntry) {
+        this.name.disable();
+      }
+    });
   }
 
   // // //  Event handlers
@@ -68,5 +134,21 @@ export class VocabulariesFormComponent extends EntitiesFormComponent<Vocabulary>
    */
   onDialogDismiss() {
     this.dialogRef.close();
+  }
+
+  /**
+   * Event handler for `name-edit` checkbox to update the `name` field editability state.
+   *
+   * @param event
+   */
+  onNameEditCheckboxChange(event: Event) {
+    this.enableManualNameEntry = (event.target as HTMLInputElement).checked;
+
+    if (this.enableManualNameEntry) {
+      this.name.enable();
+    } else {
+      this.name.disable();
+      this.updateNameFromLabel();
+    }
   }
 }
